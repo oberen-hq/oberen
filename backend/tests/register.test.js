adfd070d5642293a8a215cdaf31a1f68e649e071
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-06-28 16:36:48 +0100

    user route finished

--- backend/tests/register.test.js
+++ backend/tests/register.test.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
 
+let url = "http://localhost:3000/api/auth/register";
 let url = "http://localhost:3000/api/auth/login";
 
 axios
-  .post(url, {
+  .post(url2, {
     username: "Thisismyusername",
     email: "thealfiephillips@gmail.com",
     password: "thisismypassword",
