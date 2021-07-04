24fba3b154261c648ef3fa7c2a8dee8aa4d367cc
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-07-04 14:41:27 +0100

    get organization jobs

--- backend/controllers/Job/jobs.js
+++ backend/controllers/Job/jobs.js
@@ -1,1 +1,29 @@
-const Job = require("../../models/Job");
+const Organization = require("../../models/Organization");
+
+// Get all of organizations available jobs
+
+const get_organization_jobs = async (req, res) => {
+  const organizationId = req.params.id;
+  let jobs = [];
+  try {
+    const organization = Organization.findById(organizationId);
+
+    for (let index = 0; index < organization.offeringJobs.length; index++) {
+      jobs.push(organization.offeringJobs[index]);
+    }
+
+    if (!job.length) {
+      return res.status(200).json({
+        data: "No currrent jobs.",
+      });
+    }
+
+    return res.status(200).json({
+      data: jobs,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Internal Server Error.",
+    });
+  }
+};
