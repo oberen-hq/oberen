0b288253b5f5c3c81ef6acfcf8c0af1311cc1eb5
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-08-08 14:04:06 +0100

    text transitions

--- landing/components/Hero/index.tsx
+++ landing/components/Hero/index.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import styles from "./Hero.module.css";
 
+import TextLoop from "react-text-loop";
+
+const adjectives = ["Test", "Yes", "No", "Lol"];
+
 class Hero extends React.Component {
   render() {
     return (
       <React.Fragment>
         <div className={styles.hero__background}>
-          <div className={styles.hero__container} id="home"></div>
+          <div className={styles.hero__container} id="home">
+            <div className={styles.hero__content}>
+              <h1 className={styles.hero__slogan}>
+                The place for developers to&nbsp;
+                <TextLoop interval={2000}>
+                  {adjectives.map((word, index) => (
+                    <span key={index} className={styles.hero__slogan_adjective}>
+                      {word}
+                    </span>
+                  ))}
+                </TextLoop>
+                software.
+              </h1>
+              <p className={styles.hero__description}>
+                Blah blah blah, lorem ipsum don't know what to say here ya de ya
+                de yada.
+              </p>
+            </div>
+          </div>
         </div>
       </React.Fragment>
     );
