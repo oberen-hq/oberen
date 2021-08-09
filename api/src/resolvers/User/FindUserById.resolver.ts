3886f2bb008b553526861ecc4f51fa9e64fb8141
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-08-09 11:16:24 +0100

    findUserById

--- api/src/resolvers/User/FindUserById.resolver.ts
+++ api/src/resolvers/User/FindUserById.resolver.ts
@@ -4,3 +4,17 @@ import LocalUserRepo from "../../db/LocalUserRepo";
 import { User } from "../../resolver-types/models/User";
 import FindUserByIdArgs from "./args/FindUserByIdArgs";
 import executeOrFail from "../../utils/executeOrFail";
+
+const localUser = new LocalUserRepo();
+
+@Resolver()
+export default class FindUserById {
+  @Query(() => User)
+  async findUserById(
+    @Arg("args") args: FindUserByIdArgs
+  ): Promise<User | ApolloError> {
+    return executeOrFail(async () => {
+      return localUser.findUserById(args.id);
+    });
+  }
+}
