6778112a3e6c45c2abe40c713b351981bec0bd29
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-08-22 17:23:47 +0100

    change url

--- landing/pages/index.tsx
+++ landing/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
       <Meta
         title="Oberon"
         description="Oberon is a brand new open source platform which allows you to hire temporary employers, or become a temporary employee to try new things; see what you like in a job!"
-        url="https://oberon.sh"
+        url="https://oberon-test.vercel.app"
         keywords="open source, oberon, temporary work, organizations, discovery, employment"
       />
       <ToastContainer
