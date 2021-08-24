e681810b5b4ff9ae899b8d0b3d217e5b1a26756d
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-08-24 01:31:11 +0100

    test

--- oberon__landing/pages/_app.tsx
+++ oberon__landing/pages/_app.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import "~/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+
+function onChange(e: any) {
+  console.log(e);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
-      <Head>{/* You found me! */}</Head>
-      <Component {...pageProps} />
+      <GoogleReCaptchaProvider
+        reCaptchaKey="6Le2Nx4cAAAAADwuAZOwfA3DFOQ7JJifvJE2KISM"
+        language="en"
+      >
+        <Head>{/* You found me! */}</Head>
+        <Component {...pageProps} />
+      </GoogleReCaptchaProvider>
     </React.Fragment>
   );
 }
