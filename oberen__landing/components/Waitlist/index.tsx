83577762f04139418fd44a3b004c84c4978132cd
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-08-25 11:54:59 +0100

    emailing, temporarily disabled

--- oberen__landing/components/Waitlist/index.tsx
+++ oberen__landing/components/Waitlist/index.tsx
@@ -2,11 +2,27 @@ import React, { useState } from "react";
 import styles from "./Waitlist.module.css";
 import FirebaseManager from "utils/firebaseManager";
 import toast from "react-hot-toast";
+import emailjs, { init } from "emailjs-com";
 
 const firebase = new FirebaseManager();
 
 const error = (msg: any) => toast.error(msg);
 const success = (email: string) => toast.success(`${email} waitlisted 🔥.`);
+
+init("user_bHXqZaAY9AYBC9P0KrB4y");
+
+function sendEmail(e: any) {
+  e.preventDefault();
+  console.log("Email would be sent but its capped so um yeah lol.");
+  // emailjs
+  //   .sendForm("service_l85s7ab", "template_3s09cax", e.target)
+  //   .then(function (response) {
+  //     console.log(`SUCCESS: ${response.status} - ${response.text}`);
+  //   })
+  //   .catch(function (err) {
+  //     console.error(err);
+  //   });
+}
 
 async function handleSubmit(email: string) {
   const result = await firebase.validateEmail(email);
@@ -14,6 +30,9 @@ async function handleSubmit(email: string) {
   if (result === "success") {
     firebase.setEmail(email);
     success((email = email));
+    setTimeout(() => {
+      window.location.reload();
+    }, 1500);
   } else {
     error(result);
   }
@@ -24,10 +43,7 @@ export default function Waitlist() {
 
   return (
     <React.Fragment>
-      <form
-        className={styles.waitlist__form}
-        onSubmit={(e) => e.preventDefault()}
-      >
+      <form className={styles.waitlist__form} onSubmit={sendEmail}>
         <div className={styles.waitlist__form_container}>
           <label
             htmlFor="email"
@@ -37,7 +53,9 @@ export default function Waitlist() {
           </label>
           <input
             className={styles.waitlist__form_container_input}
-            placeholder="eg: test@gmail.com"
+            placeholder="example@email.com"
+            name="email"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
