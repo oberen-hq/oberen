1e946cdeea1c5e081b0abe644836104766950845
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-08-28 08:11:31 +0100

    like post resolver

--- oberen__api/src/resolvers/Post/LikeUnlikePost.resolver.ts
+++ oberen__api/src/resolvers/Post/LikeUnlikePost.resolver.ts
@@ -1,4 +1,37 @@
+import { ApolloError } from "apollo-server-core";
 import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
 import { Post, User } from "../../resolver-types/models";
 import { Context } from "../../types";
 import LikeUnlikePostArgs from "./args/LikeUnlikePostArgs";
+
+@Resolver()
+export default class LikeUnlikePostResolver {
+  @Mutation(() => Post)
+  async likePost(
+    @Arg("args") { postId }: LikeUnlikePostArgs,
+    @Ctx() { req, prisma }: Context
+  ): Promise<Post | ApolloError> {
+    const userId = "randomTextbecauseauthdontworkyet";
+
+    const likedPost = prisma.post.update({
+      where: {
+        id: postId,
+      },
+      data: {
+        likers: {
+          connect: [
+            {
+              id: userId,
+            },
+          ],
+        },
+      },
+    });
+
+    if (likedPost) {
+      return likedPost;
+    } else {
+      throw new ApolloError("Error liking post", "liked_post_error");
+    }
+  }
+}
