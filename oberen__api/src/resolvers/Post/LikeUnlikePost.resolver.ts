e597251d2dda1246cd4af5fc32e3b2a6c5489897
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-08-28 08:04:18 +0100

    like and unlike post args

--- oberen__api/src/resolvers/Post/LikeUnlikePost.resolver.ts
+++ oberen__api/src/resolvers/Post/LikeUnlikePost.resolver.ts
@@ -1,3 +1,4 @@
 import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
 import { Post, User } from "../../resolver-types/models";
 import { Context } from "../../types";
+import LikeUnlikePostArgs from "./args/LikeUnlikePostArgs";
