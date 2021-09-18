3c39b7fa33c0653d0dd0637ae8da79af42eaf856
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-09-18 11:15:30 +0100

    create hastag resolver class

---
+++ oberen__api/src/resolvers/Hashtag/CreateHashtag.resolver.ts
@@ -0,0 +1,27 @@
+// IMPORTS
+
+import CreateHashtagArgs from "./args/CreateHashtagArgs";
+import HashtagRepo from "../../db/HashtagRepo";
+
+import { ApolloError } from "apollo-server-express";
+import { Resolver, Mutation, Arg, Ctx } from "type-graphql";
+import { IsAuthenticated } from "../../middleware/isAuthenticated.middleware";
+import { Context } from "../../types";
+import { Hashtag } from "../../resolver-types/models";
+
+// CODE
+
+const hashtag = new HashtagRepo()
+
+@Resolver()
+export default class CreateHashtagResolver {
+  @IsAuthenticated() // Middleware
+  @Mutation(() => Hashtag) // Set response for resolver
+  async createPost(
+    @Arg("args") args: CreateHashtagArgs,
+    @Ctx() { req }: Context,
+  ): Promise<Hashtag | ApolloError> {
+    const user = req.user; // Get current user
+    return await hashtag.create(user.id, args); // Create hashtag
+  }
+}
