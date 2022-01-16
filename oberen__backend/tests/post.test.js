9527e16ed02e650fb9ae4dd40d15d38750718e7d
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2022-01-16 22:28:40 +0000

    refactor(oberen): cleanup file tree

--- oberen__backend/tests/post.test.js
+++
@@ -1,8 +0,0 @@
-const request = require("supertest");
-const app = require("../index");
-
-const baseRoute = "/";
-
-describe("post", () => {
-  it("creates a new post");
-});
--- oberen__cli/api/signIn.js
+++
@@ -1,6 +0,0 @@
-const axios = require("axios");
-const config = require("../config");
-
-module.exports = async (options) => {
-  return await axios.post(`${config.baseUrl}/auth/login`, options);
-};
