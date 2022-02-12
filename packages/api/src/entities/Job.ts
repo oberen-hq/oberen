42db49d14681dcd8c853fa947c7402db042459ac
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2022-02-12 16:09:57 +0000

    Job entity

---
+++ packages/api/src/entities/Job.ts
@@ -0,0 +1,37 @@
+import { ObjectType, Field } from "type-graphql";
+import {
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  BaseEntity,
+  OneToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+
+import { User } from "./";
+
+@ObjectType()
+@Entity()
+export default class Job extends BaseEntity {
+  @Field()
+  @PrimaryColumn({ default: () => uuid() })
+  id!: string;
+
+  @Field()
+  @Column()
+  creatorId: number;
+
+  @Field(() => User)
+  @OneToOne(() => User, (user) => user.profile)
+  creator: User;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
