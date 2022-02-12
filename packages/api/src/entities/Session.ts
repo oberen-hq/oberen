69800ae6d37026471cbe5422acf64ccbdaf3816c
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2022-02-12 16:01:07 +0000

    feat: Session entity

---
+++ packages/api/src/entities/Session.ts
@@ -0,0 +1,37 @@
+import { ObjectType, Field } from "type-graphql";
+import {
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  BaseEntity,
+  OneToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+
+import { User } from "./";
+
+@ObjectType()
+@Entity()
+export default class Session extends BaseEntity {
+  @Field()
+  @PrimaryColumn({ default: () => uuid() })
+  id!: string;
+
+  @Field()
+  @Column()
+  userId: number;
+
+  @Field(() => User)
+  @OneToOne(() => User, (user) => user.profile)
+  user: User;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
