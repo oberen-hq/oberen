86b5c22b38178dce746261417cfd94c2a76a608b
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2022-02-14 12:52:48 +0000

    update runtime script
    Change environment variables option.

--- packages/api/run.sh
+++ packages/api/run.sh
@@ -11,6 +11,23 @@ fi
 
 sleep 1
 
+echo Would you like to edit any environment variables before moving on? [y/n]
+read RESPONSE
+
+if ["$RESPONSE" = "y" ]; then
+    echo Enter the variables you want to change. ETC -> foo=bar
+    read ENVIRONMENT
+    
+    for var in ENVIRONMENT; do
+        export var[0]=var[1]
+    done
+    
+    sleep 1
+    echo Environment variables have now be changed...
+fi
+
+sleep 3
+
 echo What version will this be? WARNING: This will overwrite the existing version: 
 read VERSION
 
